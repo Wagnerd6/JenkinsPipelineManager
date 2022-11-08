866dc8936acc9ee6aa007513c323f21ff7074be8
Wagnerd6 <wennisdagner@yahoo.de>
2022-11-08 17:00:40 +0100

    try execute function

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -1,4 +1,5 @@
 def call(String configJsonPath = "PipelineConfig.json", def body) {
     println("Debug: JenkinsPipelineCpp is started from JenkinsPieplineManager! (2)")
+    "pwd".execute()
     //json = readJSON(file: configJsonPath, returnPojo: true)
 }
