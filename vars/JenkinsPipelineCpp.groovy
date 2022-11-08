c2615fff9c0b08ef3a5bd955abcf941491e52700
Dennis <wennisdagner@yahoo.de>
2022-11-08 15:35:06 +0100

    Adjust pipeline script for JenkinsPipelineCpp

--- vars/JenkinsPipelineCpp.groovy
+++
@@ -1,4 +0,0 @@
-def call(String configJsonPath = "PipelineConfig.json", def body) {
-    println("Debug: startPipeline executed")
-    json = readJSON(file: configJsonPath, returnPojo: true)
-}
---
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -0,0 +1,4 @@
+def call(String configJsonPath = "PipelineConfig.json", def body) {
+    println("Debug: JenkinsPipelineCpp is started from JenkinsPieplineManager!")
+    //json = readJSON(file: configJsonPath, returnPojo: true)
+}
