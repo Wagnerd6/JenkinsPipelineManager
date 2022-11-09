b17407947455b004fd5463c8f82de0133246be07
Wagnerd6 <wennisdagner@yahoo.de>
2022-11-09 08:51:33 +0100

    Test if echo works in shared libraries

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -1,5 +1,4 @@
 def call(String configJsonPath = "PipelineConfig.json", def body) {
     println("Debug: JenkinsPipelineCpp is started from JenkinsPieplineManager! (2)")
-    "pwd".execute()
-    //json = readJSON(file: configJsonPath, returnPojo: true)
+    echo("Does echo work in shared libraries because of Basic Pipeline Steps?")
 }
