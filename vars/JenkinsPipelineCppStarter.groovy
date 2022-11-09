7bb02d07db1c2ef2bf8b1220b25f5cc911ae2253
dwagner <dwagner@sedenius.com>
2022-11-09 09:55:24 +0100

    Add JenkinsPipelineCpp logic to shared library script

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -1,8 +1,44 @@
 def call(String configJsonPath = "PipelineConfig.json", def body) {
-    println("Debug: JenkinsPipelineCpp is started from JenkinsPieplineManager!")
     node {
-        echo "Does echo work in shared libraries because of Basic Pipeline Steps?"
-        pwd
-        sh "cmake --version"
+        // Git checkout before load source the file
+        checkout scm
+
+        stage('Test Pipeline Utility Steps plugin') {
+            def json = readJSON(file: "PipelineConfig.json", returnPojo: true)
+            println(json)
+        }
+
+        stage('Build') {
+            echo "Build stage"
+            sh 'rm -rf git_sed_jenkins_pipeline'
+            sh 'git clone ssh://gitea@homer.sedenius.com:2222/ENGINEERING/git_sed_jenkins_pipeline.git'
+            dir("git_sed_jenkins_pipeline") {
+                dir("lib/googletest") {
+                    //sh 'rm -rf googletest'
+                    //sh 'git clone https://github.com/google/googletest'
+                }
+                dir("build") {
+                    sh 'cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..'
+                    sh 'cmake --build .'
+                }
+            }
+
+        }
+
+        stage('Test') {
+            echo "Test stage"
+            //if(params.RUN_TESTS) {
+            dir('git_sed_jenkins_pipeline/build') {
+                sh 'bin/Product 7 8 2'
+                sh 'bin/Product_test'
+            }
+            //}
+        }
+
+        stage('Deploy') {
+            echo "Deploy stage"
+        }
     }
 }
+
+
