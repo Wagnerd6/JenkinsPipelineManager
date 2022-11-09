dcfb254e34c09862558053ba7d8916925e22adf1
Wagnerd6 <wennisdagner@yahoo.de>
2022-11-09 09:22:19 +0100

    Change to typical Jenkinsfile syntax, add shell command

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -1,8 +1,8 @@
 def call(String configJsonPath = "PipelineConfig.json", def body) {
     println("Debug: JenkinsPipelineCpp is started from JenkinsPieplineManager!")
     node {
-        echo("Does echo work in shared libraries because of Basic Pipeline Steps?")
-        pwd()
-        println(pwd())
+        echo "Does echo work in shared libraries because of Basic Pipeline Steps?"
+        pwd
+        sh "cmake --version"
     }
 }
