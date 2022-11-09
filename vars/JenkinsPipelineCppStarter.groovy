a35a97fcc679cc65655c716ae9c49093b7001a17
Wagnerd6 <wennisdagner@yahoo.de>
2022-11-09 10:01:40 +0100

    Adjust build script not calling SED logic

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -10,25 +10,16 @@ def call(String configJsonPath = "PipelineConfig.json", def body) {
 
         stage('Build') {
             echo "Build stage"
-            sh 'rm -rf git_sed_jenkins_pipeline'
-            sh 'git clone ssh://gitea@homer.sedenius.com:2222/ENGINEERING/git_sed_jenkins_pipeline.git'
-            dir("git_sed_jenkins_pipeline") {
-                dir("lib/googletest") {
-                    //sh 'rm -rf googletest'
-                    //sh 'git clone https://github.com/google/googletest'
-                }
-                dir("build") {
-                    sh 'cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..'
-                    sh 'cmake --build .'
-                }
+            dir("build") {
+                sh 'cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..'
+                sh 'cmake --build .'
             }
-
         }
 
         stage('Test') {
             echo "Test stage"
             //if(params.RUN_TESTS) {
-            dir('git_sed_jenkins_pipeline/build') {
+            dir('build') {
                 sh 'bin/Product 7 8 2'
                 sh 'bin/Product_test'
             }
