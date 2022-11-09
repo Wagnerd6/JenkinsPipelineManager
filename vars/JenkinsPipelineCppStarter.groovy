95d3638a3bd321097c30a6c68e9d95248d5c4c58
Wagnerd6 <wennisdagner@yahoo.de>
2022-11-09 09:13:03 +0100

    Add node block to shared library

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -1,6 +1,8 @@
 def call(String configJsonPath = "PipelineConfig.json", def body) {
     println("Debug: JenkinsPipelineCpp is started from JenkinsPieplineManager!")
-    echo("Does echo work in shared libraries because of Basic Pipeline Steps?")
-    pwd()
-    println(pwd())
+    node {
+        echo("Does echo work in shared libraries because of Basic Pipeline Steps?")
+        pwd()
+        println(pwd())
+    }
 }
