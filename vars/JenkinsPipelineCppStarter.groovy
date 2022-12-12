05f8fd6122387089898320d721366d9204a5060b
Wagnerd6 <wennisdagner@yahoo.de>
2022-12-12 11:52:29 +0100

    Remove all

--- vars/JenkinsPipelineCppStarter.groovy
+++ vars/JenkinsPipelineCppStarter.groovy
@@ -4,26 +4,7 @@ def call(String configJsonPath = "PipelineConfig.json", def body) {
         // checkout scm
         stage('Build') {
             echo "Build stage"
-            dir("build") {
-                sh 'cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..'
-                sh 'cmake --build .'
-            }
         }
-
-        stage('Test') {
-            echo "Test stage"
-            //if(params.RUN_TESTS) {
-            dir('build') {
-                sh 'bin/Product 7 8 2'
-                sh 'bin/Product_test'
-            }
-            //}
-        }
-
-        stage('Deploy') {
-            echo "Deploy stage"
-        }
-    }
 }
 
 return this
